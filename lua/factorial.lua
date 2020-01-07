5038af264637e455408daaa3ea29823707cb33a5
Ben Thornton <smenjas@users.noreply.github.com>
2020-01-06 21:13:21 -0800

    Add Tcl & Lua.

---
+++ lua/factorial.lua
@@ -0,0 +1,14 @@
+#!/usr/bin/env lua
+
+-- defines a factorial function
+function fact (n)
+  if n == 0 then
+    return 1
+  else
+    return n * fact(n-1)
+  end
+end
+
+print("Enter a number:")
+a = io.read("*number")
+print(fact(a))
